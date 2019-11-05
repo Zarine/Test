d95b1f0857df48bb0996722f37b1d9042b11c557
Zarine <nicommki@hotmail.fr>
2019-11-05 16:12:07 +0100

    create table test

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const Discord = require('discord.js');
 const discordClient = new Discord.Client();
 const PREFIX = "!";
 
-
 console.log(`hello`);
 
 const { Client } = require('pg');
@@ -16,7 +15,7 @@ const client = new Client({
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+client.query('CREATE TABLE table_name (id VARCHAR(25) UNIQUE PRIMARY KEY, data TEXT);', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
