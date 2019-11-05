2be32783cc69169e3ace4c0af6f77f1fbf3a5968
Zarine <nicommki@hotmail.fr>
2019-11-05 16:23:25 +0100

    update queries

--- bot.js
+++ bot.js
@@ -15,18 +15,18 @@ const client = new Client({
 
 client.connect();
 
-client.query('CREATE TABLE table_name (id VARCHAR(25) UNIQUE PRIMARY KEY, data TEXT);', (err, res) => {
+client.query("CREATE TABLE table_name (id VARCHAR(25) UNIQUE PRIMARY KEY, data TEXT);", (err, res) => {
   if (err) console.log('la table existe deja');
   client.end();
 });
 
-client.query('INSERT INTO table_name (id, data) VALUES ("123", "{}");', (err, res) => {
-  if (err) console.log('insert fail');
+client.query("INSERT INTO table_name (id, data) VALUES ('123', '{}');", (err, res) => {
+  if (err) console.log('insert fail: ' + err);
   client.end();
 });
 
-client.query('SELECT data FROM table_name WHERE id = "123";', (err, res) => {
-  if (err) console.log('select fail');
-  console.log('response:' + res);
+client.query("SELECT data FROM table_name WHERE id = '123';", (err, res) => {
+  if (err) { console.log('select fail: ' + err); }
+  else { console.log('response:' + res); }
   client.end();
 });
