70c0a032f48b31e1a31a907bad5c2ac5651a6c89
Zarine <nicommki@hotmail.fr>
2019-11-06 08:12:59 +0100

    try json insert

--- bot.js
+++ bot.js
@@ -15,18 +15,39 @@ const client = new Client({
 
 client.connect();
 
-client.query("CREATE TABLE table_name (id VARCHAR(25) UNIQUE PRIMARY KEY, data TEXT);", (err, res) => {
+client.query("CREATE TABLE personnage (id VARCHAR(25) UNIQUE PRIMARY KEY, data JSONB NOT NULL);", (err, res) => {
   if (err) console.log('la table existe deja');
 });
+
+async function get(id) {
+  const [row] = await db.query(
+    sql`
+      SELECT data
+      FROM personnage
+      WHERE id=${id}
+    `
+  );
+  return row ? row.data : null;
+}
+async function set(id, value) {
+  await db.query(sql`
+    INSERT INTO personnage (id, data)
+    VALUES (${id}, ${value})
+    ON CONFLICT id
+    DO UPDATE SET data = EXCLUDED.data;
+  `);
+}
 
 var data = {};
 data.key = "123";
 data.other = "bonjour";
 
-var insertQuery = "INSERT INTO table_name (id, data) VALUES ('12347', " + JSON.stringify(data) + ");";
-client.query(insertQuery, (err, res) => {
-  if (err) console.log('insert fail: ' + err);
-});
+await set('12347', data);
+
+//var insertQuery = "INSERT INTO table_name (id, data) VALUES ('12347', " + JSON.stringify(data) + ");";
+//client.query(insertQuery, (err, res) => {
+//  if (err) console.log('insert fail: ' + err);
+//});
 
 client.query("SELECT data FROM table_name WHERE id = '12347';", (err, res) => {
   if (err) { console.log('select fail: ' + err); }
