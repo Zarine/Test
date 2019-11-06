f483b6f5566f332e4e2a2ed1e073ecc57b11689e
Zarine <nicommki@hotmail.fr>
2019-11-06 08:29:04 +0100

    new try on insert

--- bot.js
+++ bot.js
@@ -20,35 +20,16 @@ client.query("CREATE TABLE personnage (id VARCHAR(25) UNIQUE PRIMARY KEY, data J
   if (err) console.log('la table existe deja');
 });
 
-async function get(id) {
-  const [row] = await client.query(
-    sql`
-      SELECT data
-      FROM personnage
-      WHERE id=${id}
-    `
-  );
-  return row ? row.data : null;
-}
-async function set(id, value) {
-  await client.query(sql`
-    INSERT INTO personnage (id, data)
-    VALUES (${id}, ${value})
-    ON CONFLICT id
-    DO UPDATE SET data = EXCLUDED.data;
-  `);
-}
-
 var data = {};
 data.key = "123";
 data.other = "bonjour";
 
 set('12347', data);
 
-//var insertQuery = "INSERT INTO table_name (id, data) VALUES ('12347', " + JSON.stringify(data) + ");";
-//client.query(insertQuery, (err, res) => {
-//  if (err) console.log('insert fail: ' + err);
-//});
+var insertQuery = "INSERT INTO table_name (id, data) VALUES ('12347', '" + JSON.stringify(data) + "');";
+client.query(insertQuery, (err, res) => {
+  if (err) console.log('insert fail: ' + err);
+});
 
 client.query("SELECT data FROM table_name WHERE id = '12347';", (err, res) => {
   if (err) { console.log('select fail: ' + err); }
